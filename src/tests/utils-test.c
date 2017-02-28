935033f2dd7f98525730d176b9fda02ecb09defc
Chad Estioco <chadestioco@gmail.com>
2017-02-28 23:24:30 +0800

    Test arrsize.

--- src/tests/utils-test.c
+++ src/tests/utils-test.c
@@ -4,4 +4,6 @@
 Test(suite_name, test_name) {
     int test1[] = {1};
     cr_assert(max(test1, 1) == 0, "Singleton max");
+    int size3[] = {1, 2, 3};
+    cr_assert(arrsize(size3) == 3, "Happy arrzie");
 }
