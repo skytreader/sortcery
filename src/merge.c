5a3522494b422331628ec9c454624c99abdccae8
Chad Estioco <chadestioco@gmail.com>
2017-03-17 15:52:25 +0800

    Add mergesort files.
    This is not even doing much yet but that is only because, the way I see it, this
    is going to be very dependent on a bug-free shift method.

---
+++ src/merge.c
@@ -0,0 +1,29 @@
+#include "utils.h"
+
+/**
+Assuming that the subarrays described by the index ranges
+    
+    [p1_origin, p2_origin)
+    [p2_origin, p2_limit)
+
+are sorted, this method merges them into one sorted array spanning [p1_origin,
+p2_limit) in the provided array.
+
+@param arr
+@param limit
+    The length of the array.
+@param p1_origin
+@param p2_origin
+@param p2_limit
+*/
+void merge(int[] arr, int p1_origin, int p2_origin, int p2_limit){
+    int i = p1_origin;
+    int j = p2_origin;
+    
+    while(i < p2_origin && j < p2_limit){
+        if(arr[i] < arr[j]){
+            i++;
+        } else if(arr[i] > arr[j]){
+        }
+    }
+}
---
+++ src/merge.h
@@ -0,0 +1,1 @@
+void merge(int[] arr, int limit, int p1_origin, int p2_origin, int p2_limit);
