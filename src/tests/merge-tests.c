d08e7e492d0eca73d9ed359009c570691bf74d3e
Chad Estioco <chadestioco@gmail.com>
2017-04-07 04:17:54 +0800

    Two more merge tests.

--- src/tests/merge-tests.c
+++ src/tests/merge-tests.c
@@ -25,3 +25,26 @@ Test(merge_tests, large_merge){
     merge(test, testlimit, 0, 5, testlimit);
     cr_assert_eq(memcmp(test, expected, testlimit_bytes), 0, "Post-call check");
 }
+
+Test(merge_tests, large_submerge){
+    int test[] = {1, 2, 4, 7, 11, 2, 3, 6, 10, 1, 2, 3, 4};
+    int expected[] = {1, 2, 2, 3, 4, 6, 7, 10, 11, 1, 2, 3, 4};
+    int testlimit = arrsize(test);
+    int testlimit_bytes = sizeof(test);
+
+    cr_assert_eq(arrsize(test), arrsize(expected), "Sanity check");
+    cr_assert_neq(memcmp(test, expected, testlimit_bytes), 0, "Pre-call check");
+    merge(test, testlimit, 0, 5, 9);
+    cr_assert_eq(memcmp(test, expected, testlimit_bytes), 0, "Post-call check");
+}
+
+Test(merge_tests, same_elements_no_movement){
+    int test[] = {1, 1, 1, 2, 2, 2, 1, 2, 3};
+    int expected[] = {1, 1, 1, 2, 2, 2, 1, 2, 3};
+    int testlimit = arrsize(test);
+    int testlimit_bytes = sizeof(test);
+
+    cr_assert_eq(arrsize(test), arrsize(expected), "Sanity check");
+    merge(test, testlimit, 0, 3, 6);
+    cr_assert_eq(memcmp(test, expected, testlimit_bytes), 0, "Post-call check");
+}
