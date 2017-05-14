895999edbfc6e5b815ce9e1f8182489df5403194
Chad Estioco <chadestioco@gmail.com>
2017-05-15 00:23:53 +0800

    Completely clean-up bstree.c for all it holds.

--- src/bstree.c
+++ src/bstree.c
@@ -1,4 +1,7 @@
+#include <stdbool.h>
+#include <string.h>
 #include "bstree.h"
+#include "utils.h"
 
 void bst_insert(btree *bt, int data){
 }
@@ -18,16 +21,24 @@ pointer to a structure) because we don't ever want to modify that structure. On
 one hand, assuming I understand C correctly, this is a good idea. On the other
 hand, this is wasteful in memory---I should just assert that the btree is
 unmodified after this function is called.
+
+@param bt
+    The binary tree to traverse. Note the caveats above.
+@param inorder
+    int array which, after this function is called, will hold the inorder
+    traversal of the given binary tree. This is assumed to be allocated enough
+    to hold all the nodes in bt.
 */
-int* traverse_inorder(btree bt){
-    static int inorder[bt.count];
-    bool lvisited[bt.count] = {false};
-    bool rvistied[bt.count] = {false};
+int* traverse_inorder(btree bt, int inorder[]){
+    bool lvisited[bt.node_count];
+    memset(lvisited, false, bt.node_count * sizeof(bool));
+    bool rvisited[bt.node_count];
+    memset(rvisited, false, bt.node_count * sizeof(bool));
     int inorder_idx = 0;
     btree *roamer = &bt;
 
-    while(inorder_idx != bt.count){
-        if(roamer.lchild == NULL){
+    while(inorder_idx != bt.node_count){
+        if(roamer->lchild == NULL){
         }
     }
 
